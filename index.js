5e3d7e2d85d1faac0009b9117646c240138920f7
eniodev <enioblvck@gmail.com>
2024-10-08 03:43:25 +0100

    chore: added setup quetions array

--- index.js
+++ index.js
@@ -9,6 +9,8 @@ const rl = readline.createInterface({
   output: process.stdout
 });
 
+const question = (str) => new Promise((cb) => rl.question(str, cb));
+
 let stylesFolder = "CSS";
 let scriptsFolder = "JS";
 let htmlEntryFile = "index.html";
@@ -25,6 +27,50 @@ const projectFileTree = {
   }
 }
 
-init(projectFileTree);
+const setupQuestions = [
+  {
+    question: "Html file name: (root folder)",
+    default: "index.html"
+  },  
+  {
+    question: "Css folder name: (css)",
+    default: "css",
+    child: {
+      question: "Css file name: (styles.css)",
+      default: "styles.css"
+    }
+  },
+  {
+    question: "Javascript folder name: (js)",
+    default: "js",
+    child: {
+      question: "Javascript file name: (scripts.js)",
+      default: "scripts.js"
+    }
+  },
+  {
+    question: "Pick a name for your image folder: (img)",
+    default: "img"
+  }
+]
+
+const run = async () => {
+  try {
+    await new Promise(() => {
+      setupQuestions.forEach(async (option) => {
+        const subFolder = await question(option.question);
+        console.log(subFolder);
+      })
+    });
+  } catch (err) {
+    console.log(`💥 Something went wrong!! Please Try Again...${err.message}`);
+    process.exit(1);
+  }
+  finally {
+    await process.exit(0);
+  }
+}
+
+run();
 
 module.exports = projectFileTree
