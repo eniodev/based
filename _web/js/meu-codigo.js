d7bda94e541a355ea779f73ca63e6e7b67bbe675
eniodev <enioblvck@gmail.com>
2024-10-14 02:03:04 +0100

    chore: deleted uneeded folders

--- _web/js/meu-codigo.js
+++
@@ -1,9 +0,0 @@
-
-counter = 0;
-const counterBtn = document.querySelector("[data-counter]");
-counterBtn.firstElementChild.innerText += counter;
- 
-counterBtn.addEventListener("click", () => {
-counterBtn.firstElementChild.innerText = ++counter;
-});
-    
